d5068365629c5c144170fef1ef37cda6295c4910
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2020-07-17 12:19:03 +0530

    Update LiquorService.java

--- src/main/java/LiquorService.java
+++ src/main/java/LiquorService.java
@@ -15,11 +15,11 @@ public class LiquorService {
             brands.add("Adrianna Vineyard");
             brands.add(("J. P. Chenet"));
 
-        }/*else if(type.equals(LiquorType.WHISKY)){
+        }else if(type.equals(LiquorType.WHISKY)){
             brands.add("Glenfiddich");
             brands.add("Johnnie Walker");
 
-        }*/else if(type.equals(LiquorType.BEER)){
+        }else if(type.equals(LiquorType.BEER)){
             brands.add("Corona");
 
         }else {
